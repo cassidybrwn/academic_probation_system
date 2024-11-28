28b6565f8d696fc9eb8545b905715541aecd2005
Del-Piero Graham <91176519+Del-PieroGraham@users.noreply.github.com>
2024-11-27 20:39:13 -0500

    gpa_calculations.pl

--- gpa_calculations.pl
+++ gpa_calculations.pl
@@ -1,3 +1,5 @@
+% gpa_rules.pl
+
 % Default GPA threshold
 :- dynamic default_gpa/1.
 default_gpa(2.0).
@@ -14,17 +16,19 @@ calculate_totals([(GradePoints, Credits)|Rest], TotalPoints, TotalCredits) :-
     TotalPoints is RestPoints + Earned,
     TotalCredits is RestCredits + Credits.
 
-% Calculate semester GPA
-calculate_semester_gpa(Modules, GPA) :-
-    calculate_totals(Modules, TotalPoints, TotalCredits),
-    TotalCredits > 0,
-    GPA is TotalPoints / TotalCredits.
-
-% Calculate cumulative GPA (average of two semester GPAs)
-calculate_cumulative_gpa(GPA1, GPA2, CGPA) :-
-    CGPA is (GPA1 + GPA2) / 2.
-
 % Calculate GPA for a semester
 calculate_gpa(TotalGradePoints, TotalCredits, GPA) :-
     TotalCredits > 0,
     GPA is TotalGradePoints / TotalCredits.
+
+% Calculate cumulative GPA
+calculate_cumulative_gpa(GPA1, GPA2, CGPA) :-
+    CGPA is (GPA1 + GPA2) / 2.
+
+% Check academic probation
+check_academic_probation(GPA, probation) :-
+    default_gpa(DefaultGPA),
+    GPA < DefaultGPA, !.
+
+
+check_academic_probation(_, good) :- !.
